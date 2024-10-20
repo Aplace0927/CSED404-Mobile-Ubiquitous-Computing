276e333205d694414b3d0a2863e79df35cd6dd86
Aplace <aplace030927@gmail.com>
2024-10-20 13:50:43 +0900

    feat(sensor): add gravity and gyroscopes

--- HW1/app/src/main/java/com/csed433/hw1/MainActivity.kt
+++ HW1/app/src/main/java/com/csed433/hw1/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : ComponentActivity() {
     private lateinit var mSensorManager: SensorManager
     private var mSensing: Boolean = false
     private lateinit var mAccel: Sensor
+    private lateinit var mGravity: Sensor
+    private lateinit var mGyro: Sensor
     private lateinit var mSensorEventListener: SensorEventListener
     private lateinit var mWorkerThread: HandlerThread
     private lateinit var mHandlerWorker: Handler
@@ -26,7 +28,11 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
         mAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) as Sensor
+        mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) as Sensor
+        mGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) as Sensor
+
         mWorkerThread = HandlerThread("Worker Thread")
         mWorkerThread.start()
         mHandlerWorker = Handler(mWorkerThread.looper)
@@ -54,10 +60,30 @@ class MainActivity : ComponentActivity() {
                     val ts = event.timestamp
                     val values = event.values.clone()
                     runOnUiThread {
-                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(ts)
+                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(ts / 1000)    // Nanosecond -> Microsecond
                         updateTextViewValue(R.id.accel_x_value_text, values[0])
                         updateTextViewValue(R.id.accel_y_value_text, values[1])
                         updateTextViewValue(R.id.accel_z_value_text, values[2])
+                    }
+                }
+                if (event?.sensor?.type == Sensor.TYPE_GRAVITY) {
+                    val ts = event.timestamp
+                    val values = event.values.clone()
+                    runOnUiThread {
+                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(ts / 1000)    // Nanosecond -> Microsecond
+                        updateTextViewValue(R.id.gravity_x_value_text, values[0])
+                        updateTextViewValue(R.id.gravity_y_value_text, values[1])
+                        updateTextViewValue(R.id.gravity_z_value_text, values[2])
+                    }
+                }
+                if (event?.sensor?.type == Sensor.TYPE_GYROSCOPE) {
+                    val ts = event.timestamp
+                    val values = event.values.clone()
+                    runOnUiThread {
+                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(ts / 1000)    // Nanosecond -> Microsecond
+                        updateTextViewValue(R.id.gyro_x_value_text, values[0])
+                        updateTextViewValue(R.id.gyro_y_value_text, values[1])
+                        updateTextViewValue(R.id.gyro_z_value_text, values[2])
                     }
                 }
             }
@@ -65,9 +91,13 @@ class MainActivity : ComponentActivity() {
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
         }
         mSensorManager.registerListener(mSensorEventListener, mAccel, 10000, mHandlerWorker)
+        mSensorManager.registerListener(mSensorEventListener, mGravity, 10000, mHandlerWorker)
+        mSensorManager.registerListener(mSensorEventListener, mGyro, 10000, mHandlerWorker)
     }
 
     fun stopSensing() {
         mSensorManager.unregisterListener(mSensorEventListener, mAccel)
+        mSensorManager.unregisterListener(mSensorEventListener, mGyro)
+        mSensorManager.unregisterListener(mSensorEventListener, mGravity)
     }
 }
