49aab776650d5432a442e817113f3caef732d2a2
Aplace <aplace030927@gmail.com>
2024-10-23 20:44:32 +0900

    feat(file): file output implemented - File will be exported as `yyyyMMdd-HHmmss_[accel/gyro/grav].csv` - Under the Download directory

--- HW1/app/src/main/java/com/csed433/hw1/MainActivity.kt
+++ HW1/app/src/main/java/com/csed433/hw1/MainActivity.kt
@@ -6,14 +6,15 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorManager
 import android.hardware.SensorEventListener
+import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
 import android.widget.Button
 import android.widget.TextView
-import androidx.compose.ui.graphics.Color
-import java.io.BufferedWriter
 import java.io.File
-import java.nio.Buffer
+import java.io.FileOutputStream
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
 
@@ -26,9 +27,26 @@ class MainActivity : ComponentActivity() {
     private lateinit var mSensorEventListener: SensorEventListener
     private lateinit var mWorkerThread: HandlerThread
     private lateinit var mHandlerWorker: Handler
-    private lateinit var wFile: File
-    private lateinit var wBuffer: BufferedWriter
 
+    private lateinit var wGyroFile: File
+    private lateinit var wGyroOutputStream: FileOutputStream
+
+    private lateinit var wGravFile: File
+    private lateinit var wGravOutputStream: FileOutputStream
+
+    private lateinit var wAcclFile: File
+    private lateinit var wAcclOutputStream: FileOutputStream
+
+    private var timestamp = 0L
+    private var xGyro = 0.0f
+    private var yGyro = 0.0f
+    private var zGyro = 0.0f
+    private var xGrav = 0.0f
+    private var yGrav = 0.0f
+    private var zGrav = 0.0f
+    private var xAccl = 0.0f
+    private var yAccl = 0.0f
+    private var zAccl = 0.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,22 +68,50 @@ class MainActivity : ComponentActivity() {
         btnStartStop.setOnClickListener() {
             mSensing = !mSensing
             if (mSensing) {
-                // Initialize buffer
+
+                val wTimeString = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"))
+
+                wAcclFile = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path + "/" + wTimeString + "_accel.csv")
+                wGravFile = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path + "/" + wTimeString + "_grav.csv")
+                wGyroFile = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path + "/" + wTimeString + "_gyro.csv")
+
+                if (!wAcclFile.exists()) {
+                    wAcclFile.createNewFile()
+                }
+
+                if (!wGravFile.exists()) {
+                    wGravFile.createNewFile()
+                }
+
+                if (!wGyroFile.exists()){
+                    wGyroFile.createNewFile()
+                }
+
+                wAcclOutputStream = FileOutputStream(wAcclFile)
+                wGravOutputStream = FileOutputStream(wGravFile)
+                wGyroOutputStream = FileOutputStream(wGyroFile)
+
                 startSensing()
+                mLogging = true
+
                 btnStartStop.setBackgroundColor(0xFF_FF4081.toInt())
                 btnStartStop.setTextColor(0xFF_FFFFFF.toInt())
                 btnStartStop.setText("STOP")
+
+                btnPauseResume.setText("PAUSE")
             }
             else {
-                // Open file
-                // Write buffer data into file
-                // Close file
-                // Clear buffer
+                mLogging = false
                 stopSensing()
+
+                wAcclOutputStream.close()
+                wGravOutputStream.close()
+                wGyroOutputStream.close()
+
                 btnStartStop.setBackgroundColor(0xFF_69F0AE.toInt())
                 btnStartStop.setTextColor(0xFF_000000.toInt())
                 btnStartStop.setText("START")
-                mLogging = false
+
                 btnPauseResume.setText("RESUME")
             }
         }
@@ -87,39 +133,52 @@ class MainActivity : ComponentActivity() {
     fun startSensing() {
         mSensorEventListener = object: SensorEventListener {
             override fun onSensorChanged(event: SensorEvent?) {
-                val updateTextViewValue: (Int, Float) -> Unit = {id, value -> findViewById<TextView>(id).text = "%.4f".format(value)}
+                val getTextViewValue: (Int) -> String = {id -> findViewById<TextView>(id).text.toString()}
+                val updateTextViewValue: (Int, Float) -> Unit = {id, value -> findViewById<TextView>(id).text = "%.9e".format(value)}
                 if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-                    val ts = event.timestamp
-                    val values = event.values.clone()
+                    timestamp = (event.timestamp / 1000).toLong()    // Nanosecond -> Microsecond
+                    xAccl = event.values[0]
+                    yAccl = event.values[1]
+                    zAccl = event.values[2]
                     runOnUiThread {
-                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(ts / 1000)    // Nanosecond -> Microsecond
-                        updateTextViewValue(R.id.accel_x_value_text, values[0])
-                        updateTextViewValue(R.id.accel_y_value_text, values[1])
-                        updateTextViewValue(R.id.accel_z_value_text, values[2])
+                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(timestamp)    // Nanosecond -> Microsecond
+                        updateTextViewValue(R.id.accel_x_value_text, xAccl)
+                        updateTextViewValue(R.id.accel_y_value_text, yAccl)
+                        updateTextViewValue(R.id.accel_z_value_text, zAccl)
+                    }
+                    if (mLogging) {
+                        wAcclOutputStream.write("%d, %.9e, %.9e, %.9e\n".format(timestamp, xAccl, yAccl, zAccl).toByteArray())
                     }
                 }
                 if (event?.sensor?.type == Sensor.TYPE_GRAVITY) {
-                    val ts = event.timestamp
-                    val values = event.values.clone()
+                    timestamp = (event.timestamp / 1000).toLong()    // Nanosecond -> Microsecond
+                    xGrav = event.values[0]
+                    yGrav = event.values[1]
+                    zGrav = event.values[2]
                     runOnUiThread {
-                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(ts / 1000)    // Nanosecond -> Microsecond
-                        updateTextViewValue(R.id.gravity_x_value_text, values[0])
-                        updateTextViewValue(R.id.gravity_y_value_text, values[1])
-                        updateTextViewValue(R.id.gravity_z_value_text, values[2])
+                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(timestamp)    // Nanosecond -> Microsecond
+                        updateTextViewValue(R.id.gravity_x_value_text, xGrav)
+                        updateTextViewValue(R.id.gravity_y_value_text, yGrav)
+                        updateTextViewValue(R.id.gravity_z_value_text, zGrav)
+                    }
+                    if (mLogging) {
+                        wGravOutputStream.write("%d, %.9e, %.9e, %.9e\n".format(timestamp, xGrav, yGrav, zGrav).toByteArray())
                     }
                 }
                 if (event?.sensor?.type == Sensor.TYPE_GYROSCOPE) {
-                    val ts = event.timestamp
-                    val values = event.values.clone()
+                    timestamp = (event.timestamp / 1000).toLong()    // Nanosecond -> Microsecond
+                    xGyro = event.values[0]
+                    yGyro = event.values[1]
+                    zGyro = event.values[2]
                     runOnUiThread {
-                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(ts / 1000)    // Nanosecond -> Microsecond
-                        updateTextViewValue(R.id.gyro_x_value_text, values[0])
-                        updateTextViewValue(R.id.gyro_y_value_text, values[1])
-                        updateTextViewValue(R.id.gyro_z_value_text, values[2])
+                        findViewById<TextView>(R.id.time_t_value_text).text = "%d".format(timestamp)    // Nanosecond -> Microsecond
+                        updateTextViewValue(R.id.gyro_x_value_text, xGyro)
+                        updateTextViewValue(R.id.gyro_y_value_text, yGyro)
+                        updateTextViewValue(R.id.gyro_z_value_text, zGyro)
                     }
-                }
-                if (mLogging) {
-                    // Append sensor data to buffer
+                    if (mLogging) {
+                        wGyroOutputStream.write("%d, %.9e, %.9e, %.9e\n".format(timestamp, xGyro, yGyro, zGyro).toByteArray())
+                    }
                 }
             }
 
